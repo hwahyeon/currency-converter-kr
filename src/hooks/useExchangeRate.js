df234fd7524a62279ffda046cbf934d69ebf1026
hwahyeon <hwahyeon@knou.ac.kr>
2023-12-01 02:34:30 +0900

    다른 통화 추가

--- src/hooks/useExchangeRate.js
+++ src/hooks/useExchangeRate.js
@@ -1,27 +1,30 @@
 import { useState, useEffect } from "react";
 
-export function useExchangeRate() {
-  const [euroToKrw, setEuroToKrw] = useState(null);
+export function useExchangeRate(selectedCurrency) {
+  const [currencyToKrw, setCurrencyToKrw] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  const KRW = 'krw';
+
   useEffect(() => {
-    fetch("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/krw.min.json")
-      .then(response => {
+    const fetchExchangeRate = async (selectedCurrency, toCurrency = KRW, setRate) => {
+      try {
+        const response = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${selectedCurrency}/${toCurrency}.min.json`);
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
-        return response.json();
-      })
-      .then(data => {
-        setEuroToKrw(data.krw);
-        setIsLoading(false);
-      })
-      .catch(error => {
+        const data = await response.json();
+        setRate(data[toCurrency]);
+      } catch (error) {
         setError(error);
-        setIsLoading(false);
-      });
-  }, []);
+      }
+    };
 
-  return { euroToKrw, isLoading, error };
+    setIsLoading(true);
+    fetchExchangeRate(selectedCurrency, KRW, setCurrencyToKrw);
+    setIsLoading(false);
+  }, [selectedCurrency]);
+
+  return { currencyToKrw, isLoading, error };
 }
