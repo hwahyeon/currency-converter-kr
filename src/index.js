eaad34222d0ad9f57a73dd0db1fbecf33b623a67
hwahyeon <hwahyeon@knou.ac.kr>
2023-12-01 00:07:37 +0900

    Added a component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Exchanger from './components/Exchanger';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Exchanger />
   </React.StrictMode>
 );
 
