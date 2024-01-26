7425e49b8d11d10534e48ee9590ad163bfd9bf8e
hwahyeon <hwahyeon@knou.ac.kr>
2024-01-27 02:59:08 +0900

    Implement multilingual support with separate lang files

---
+++ src/lang/ko.js
@@ -0,0 +1,32 @@
+export const ko = {
+  header: "환율 계산기",
+  option: {
+    krw: "대한민국 원 (KRW)",
+    eur: "유로 (EUR)",
+    usd: "미국 달러 (USD)",
+    jpy: "일본 엔 (JPY)",
+    gbp: "영국 파운드 (GBP)",
+    aud: "호주 달러 (AUD)",
+    cad: "캐나다 달러 (CAD)",
+    chf: "스위스 프랑 (CHF)",
+    vnd: "베트남 동 (VND)",
+    sek: "스웨덴 크로나 (SEK)",
+    nzd: "뉴질랜드 달러 (NZD)"
+  },
+  placeholder: "금액을 입력해주세요",
+  today_price: {
+    today:"오늘",
+    is:"(은)는",
+    end:"입니다"
+  },
+  info1:
+    "일십백천만...하며 0을 세던 날은 잊으세요! 조 위까지의 한글 숫자 표기를 지원합니다. 예를 들어, 아래의 표기법 모두 사용할 수 있습니다:",
+  example: (
+    <ul>
+      <li>삼십만원</li>
+      <li>7십팔만 육천원</li>
+      <li>950000</li>
+    </ul>
+  ),
+  info2: "입력만 하세요!",
+};
