6db445928ea63b92ec015203c5c5f576e1a492d5
Janghyeon <hwahyeon@knou.ac.kr>
2024-05-31 01:22:09 +0200

    Update fetchExchangeRate for new API endpoint

--- src/hooks/useExchangeRate.js
+++ src/hooks/useExchangeRate.js
@@ -8,12 +8,14 @@ export function useExchangeRate(selectedCurrency, toCurrency) {
   useEffect(() => {
     const fetchExchangeRate = async (selectedCurrency, toCurrency, setRate) => {
       try {
-        const response = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${selectedCurrency}/${toCurrency}.min.json`);
+        // const response = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${selectedCurrency}/${toCurrency}.min.json`);
+        const response = await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${selectedCurrency}.min.json`);
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
         const data = await response.json();
-        setRate(data[toCurrency]);
+        // setRate(data[toCurrency]);
+        setRate(data[selectedCurrency][toCurrency]);
       } catch (error) {
         setError(error);
       }
